4a1673357c3f33bbed2ae64e53f53d344a031ab0
Guilherme Novak <guilherme@govakreality.com>
2024-12-12 21:16:51 -0300

    Basic Reactivity Example

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { Canvas } from "@react-three/fiber";
+import { useState } from "react";
 
 export default function App() {
+  const [lightColor, setLightColor] = useState("red");
+
+  const handleClick = () => {
+    setLightColor(lightColor === "red" ? "blue" : "red");
+  };
+
   return (
     <Canvas>
       <ambientLight intensity={0.1} />
-      <directionalLight color="red" position={[0, 0, 5]} />
-      <mesh>
+      <directionalLight color={lightColor} position={[0, 0, 5]} />
+      <mesh onClick={handleClick}>
         <boxGeometry args={[2, 2, 2]} />
         <meshStandardMaterial />
       </mesh>
