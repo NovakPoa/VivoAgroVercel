3e8236e4a0fda82b8f85cec8b75143eca661c4fd
zecazeco <9203689+zecazeco@users.noreply.github.com>
2025-02-18 21:38:18 -0300

    primary button optional icon

--- src/components/UIs/PrimaryButton/PrimaryButton.jsx
+++ src/components/UIs/PrimaryButton/PrimaryButton.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './PrimaryButton.css';
 
-const PrimaryButton = ({ text, onClick }) => {
+const PrimaryButton = ({ text, onClick, showIcon = true }) => {
   return (
     <button className="primary-button" onClick={onClick}>
-      <img src="./textures/vivo-icon.png" alt="Vivo Icon" className="button-icon" />
+      {showIcon && <img src="./textures/vivo-icon.png" alt="Vivo Icon" className="button-icon" />}
       <span className="button-text">{text}</span>
     </button>
   );
