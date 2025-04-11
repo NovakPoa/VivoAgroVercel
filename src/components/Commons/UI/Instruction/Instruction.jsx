8e4d7be5f6ba681b17326a5f149f0b53877e63da
zecazeco <9203689+zecazeco@users.noreply.github.com>
2025-04-11 09:57:26 -0300

    Scale In instruction animation

--- src/components/Commons/UI/Instruction/Instruction.jsx
+++ src/components/Commons/UI/Instruction/Instruction.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Instruction.css';
 
 const Instruction = ({ title, description, children }) => {
 
+  const [visible, setVisible] = useState(false);
+  
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setVisible(true);
+    }, 30);
+    
+    return () => clearTimeout(timer);
+  }, []);
+
   const hasExtras = React.Children.count(children) > 0;
 
   return (
-    <div className={`instruction ${hasExtras ? 'with-extras' : ''}`}>
+    <div className={`instruction ${hasExtras ? 'with-extras' : ''} ${visible ? 'visible' : 'hidden'}`}>
       <div className="instruction-content">
         <h2 className="instruction-title">{title}</h2>
         <p className="instruction-description">{description}</p>
